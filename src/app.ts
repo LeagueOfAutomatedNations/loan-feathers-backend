0cd7ceb2d039f1a0a6fa8f3fd32e8c6cca28d479
thmsn <9084377+thmsndk@users.noreply.github.com>
2021-08-07 00:17:58 +0200

    allow requests to be larger than 100kb

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,6 @@ import configuration from '@feathersjs/configuration';
 import express from '@feathersjs/express';
 import socketio from '@feathersjs/socketio';
 
-
 import { Application } from './declarations';
 import logger from './logger';
 import middleware from './middleware';
@@ -27,13 +26,15 @@ export type HookContext<T = any> = { app: Application } & FeathersHookContext<T>
 // Load app configuration
 app.configure(configuration());
 // Enable security, CORS, compression, favicon and body parsing
-app.use(helmet({
-  contentSecurityPolicy: false
-}));
+app.use(
+  helmet({
+    contentSecurityPolicy: false
+  })
+);
 app.use(cors());
 app.use(compress());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
 app.use('/', express.static(app.get('public')));
